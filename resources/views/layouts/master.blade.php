b4c5b9b918d462c1b5942ffbd65cd33eb4fecd1c
Robert <user@Users-MacBook-Air.local>
2017-05-05 11:53:58 +1200

    updated on friday

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,9 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Johnny's Dumpling House</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Monda:400,700">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700|Open+Sans+Condensed:300,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{asset('fonts/font-awesome.min.css')}}">
--- resources/views/partials/awards.blade.php
+++ resources/views/partials/awards.blade.php
@@ -1,10 +1,42 @@
 <section id="awards">
 
     <div class="container">
-        <h1 class="text-center">Awards</h1>
+        <h1 class="text-center">Reviews</h1>
         <div class="row">
-            <div class="col-lg-3 col-md-3 col-md-offset-3 col-sm-6 col-xs-12"><img class="img-circle img-responsive" src="/images/bayhospoaward2.png"></div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"><img class="img-circle img-responsive" src="/images/domainaward.png"></div>
+       
+   <div class="col-sm-12">
+       
+       <div class="content-slider">
+  <div class="slider">
+    <div class="mask">
+      <ul>
+        <li class="anim1">
+          <div class="quote">"It's a little hidden gem. So glad I searched it out. If you like dumplings you won't be disappointed. The pork buns are a little bigger than traditional, but oh so yummy!"</div>
+          <div class="source">- Tse-Sheng Ling</div>
+        </li>
+        <li class="anim2">
+          <div class="quote">"First tried these dumplings at the mount gourmet night markets insanely good along with a steamed pork bun that was magic best around!"</div>
+          <div class="source">- Sam Hide</div>
+        </li>
+        <li class="anim3">
+          <div class="quote">"Found them at WOMAD away from the other food stalls... and SO glad I did! Excellent value for an awesome feed. If you like dumplings, you will absolutely LOVE Johney's Dumpling House."</div>
+          <div class="source">- Chad Ellwood</div>
+        </li>
+        <li class="anim4">
+          <div class="quote">"Fucking amazing!!!! Best dumplings and beer and Gin n Tonic on tap!"</div>
+          <div class="source">- Hamish Hodgson</div>
+        </li>
+        <li class="anim5">
+          <div class="quote">"The pork buns are soooo good! Loved the pork and vege dumplings too, the chilli black bean sauce was really interesting. Can't wait to go back."</div>
+          <div class="source">- Sarah Waugh</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+       
+       
+   </div>
         </div>
     </div>
 </section>
